32fd71c4d5e72a0b077779f71818f4dade9dab21
mihaelastoychev <>
2021-10-30 10:03:53 +0000

    test

--- src/exercise3/AngleCalculation.java
+++ src/exercise3/AngleCalculation.java
@@ -6,23 +6,22 @@ public class AngleCalculation {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        double value;
-        char type;
+        double winkel;
+        char ch;
         do {
-          value = input.nextDouble();
-          type = input.next().charAt(0);
+          winkel = input.nextDouble();
+          ch = input.next().charAt(0);
 
-          if (type == 'r') {
-            double convertedValue = value / Math.PI * 180;
+          if (ch == 'r') {
+            double convertedValue = winkel / Math.PI * 180;
             System.out.println("Angle: " + convertedValue + "d");
 
-          }else if (type == 'd') {
-            double convertedValue = value * Math.PI / 180;
+          }else if (ch == 'd') {
+            double convertedValue = winkel * Math.PI / 180;
             System.out.println("Angle: " + convertedValue + "r");
           }
 
-        } while (type == 'r' || type == 'd' );
+        } while (ch == 'r' || ch == 'd' );
     }
 
 }
