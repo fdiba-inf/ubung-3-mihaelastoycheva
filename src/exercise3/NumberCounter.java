a53be6f0bd771179fd6a46d5ae0606b6f558cbc5
mihaelastoychev <>
2021-10-30 09:24:19 +0000

    test

--- src/exercise3/NumberCounter.java
+++ src/exercise3/NumberCounter.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class NumberCounter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
+        int number = input.nextInt();
         int positive = 0;
         int negative = 0;
-        int sum = 0;
-        int average = 0;
+        double sum = 0;
+        double average = 0;
 
-        while(n != 0){
-            if(n > 0){
+        while(number != 0){
+            if(number > 0){
                 positive++;
             } else{
                 negative++;
             }
-            n = input.nextInt();
-            sum += n;
-            average = sum / (positive + negative);
+            number = input.nextInt();
+            sum = sum + number;
         }
-
+        average = sum / (positive + negative);
+        
         System.out.println("Positive numbers: " + positive);
         System.out.println("Negative numbers: " + negative);
         System.out.println("Sum: " + sum);
